bcaa0b47281c1eb52eff641c4d097881b6f29f0c
Obasi Adande George <oageorge@tstt.co.tt>
2023-03-22 11:52:39 -0400

    fix indent

--- powertranz.lib.php
+++ powertranz.lib.php
@@ -599,54 +599,54 @@ class PowerTranzResponse {
 
 class CreditCard 
 {
-	/**
-	 * Replaces all but the first and last four digits with x's in the given credit card number
-	 *
-	 * @param int|string $cc The credit card number to mask
+    /**
+     * Replaces all but the first and last four digits with x's in the given credit card number
      * 
-	 * @return string The masked credit card number
-	 */
-	public static function mask( $cc )
-	{
-		// replace all digits with X except for the first and last four.
-		$cc = preg_replace('/(?!^.?)[0-9](?!(.){0,3}$)/', 'X', $cc);
-		
-		// Return the masked Credit Card #
-		return $cc;
-	}
+     * @param int|string $cc The credit card number to mask
+     *   
+     * @return string The masked credit card number
+     */
+    public static function mask( $cc )
+    {
+        // replace all digits with X except for the first and last four.
+        $cc = preg_replace('/(?!^.?)[0-9](?!(.){0,3}$)/', 'X', $cc);
+        
+        // Return the masked Credit Card #
+        return $cc;
+    }
 
-	/**
-	 * Add dashes to a credit card number.
-	 *
-	 * @param int|string $cc The credit card number to format with dashes.
+    /**
+     * Add dashes to a credit card number.
+     *
+     * @param int|string $cc The credit card number to format with dashes.
      * 
-	 * @return string The credit card with dashes.
-	 */
-	public static function format( $cc )
-	{
-		// Clean out extra data that might be in the cc
-		$cc = str_replace(array('-',' '),'',$cc);
+     * @return string The credit card with dashes.
+     */
+    public static function format( $cc )
+    {
+        // Clean out extra data that might be in the cc
+        $cc = str_replace(array('-',' '),'',$cc);
 
-		// Get the CC Length
-		$cc_length = strlen($cc);
+        // Get the CC Length
+        $cc_length = strlen($cc);
 
-		// Initialize the new credit card to contian the last four digits
-		$newCreditCard = substr($cc,-4);
+        // Initialize the new credit card to contian the last four digits
+        $newCreditCard = substr($cc,-4);
 
-		// Walk backwards through the credit card number and add a dash after every fourth digit
-		for ($i=$cc_length-5; $i>=0; $i--)
+        // Walk backwards through the credit card number and add a dash after every fourth digit
+        for ($i=$cc_length-5; $i>=0; $i--)
         {
-			// If on the fourth character add a dash
-			if((($i+1)-$cc_length)%4 == 0){
-				$newCreditCard = '-'.$newCreditCard;
-			}
-			// Add the current character to the new credit card
-			$newCreditCard = $cc[$i].$newCreditCard;
-		}
+            // If on the fourth character add a dash
+            if((($i+1)-$cc_length)%4 == 0){
+                $newCreditCard = '-'.$newCreditCard;
+            }
+            // Add the current character to the new credit card
+            $newCreditCard = $cc[$i].$newCreditCard;
+        }
 
-		// Return the formatted credit card number
-		return $newCreditCard;
-	}
+        // Return the formatted credit card number
+        return $newCreditCard;
+    }
 
     /**
      * Remove all non numeric characters from a credit card number
