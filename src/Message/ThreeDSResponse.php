34ce9c0d1c1cb81abb4c462463f7ea78b0beea5d
Obasi Adande George <oageorge@tstt.co.tt>
2024-01-08 17:46:41 -0400

    fix successful check for threeds transactions

--- src/Message/ThreeDSResponse.php
+++ src/Message/ThreeDSResponse.php
@@ -9,6 +9,15 @@ class ThreeDSResponse extends AbstractResponse {
      */
     public function isSuccessful()
     {
-        return (in_array($this->transactionData->IsoResponseCode, ['3D0', '3D1'])) ? true : false;
+        if (in_array($this->transactionData->IsoResponseCode, ['3D0', '3D1']))
+        {
+            if (in_array($this->transactionData->RiskManagement->ThreeDSecure->Eci, ['01','02','05','06'])) {
+                return isset($this->transactionData->SpiToken) ? true : false;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
 }
