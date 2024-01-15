be150333c603bad3542684bc8c61f34d94ed2988
Obasi Adande George <oageorge@tstt.co.tt>
2024-01-15 10:59:45 -0400

    use in builtin constants

--- src/PowerTranz.php
+++ src/PowerTranz.php
@@ -506,12 +506,20 @@ class PowerTranz implements PowerTranzInterface {
         $url = "{$this->getEndpoint()}{$api}";
 
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 150); 
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+
+        if ($method == 'POST') {
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+        } elseif ($method == 'GET') {
+            curl_setopt($ch, CURLOPT_HTTPGET, true);
+        } else {
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+        }
+
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
